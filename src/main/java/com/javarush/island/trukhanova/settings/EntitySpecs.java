b28693b48f6636fefc9a0b532243f97095e6445b
Trukhanova03 <trukhanovamasha@gmail.com>
2025-11-19 18:01:21 +0300

    ADD EntitySpecs

--- src/main/java/com/javarush/island/trukhanova/settings/EntitySpecs.java
+++ src/main/java/com/javarush/island/trukhanova/settings/EntitySpecs.java
@@ -1,4 +1,20 @@
 package com.javarush.island.trukhanova.settings;
 
 public class EntitySpecs {
+    public final double maxWeight;
+    public final int maxSpeed;
+    public final double maxSatiation;
+    public final int maxPerLocation;
+    public final String displaySymbol;
+    public final int reproduceCount;
+
+    public EntitySpecs(double weight, int speed, double satiation, int maxLoc, String symbol, int reproduceCount) {
+        this.maxWeight = weight;
+        this.maxSpeed = speed;
+        this.maxSatiation = satiation;
+        this.maxPerLocation = maxLoc;
+        this.displaySymbol = symbol;
+        this.reproduceCount = reproduceCount;
+    }
 }
+
