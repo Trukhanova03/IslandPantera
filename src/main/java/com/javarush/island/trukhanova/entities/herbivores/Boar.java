e1b045a9833a05c13b639da7eea29e46304b0139
Trukhanova03 <trukhanovamasha@gmail.com>
2025-11-20 18:08:05 +0300

    ADD all herbivores

---
+++ src/main/java/com/javarush/island/trukhanova/entities/herbivores/Boar.java
@@ -0,0 +1,16 @@
+package com.javarush.island.trukhanova.entities.herbivores;
+
+import com.javarush.island.trukhanova.entities.base.EatingHelper;
+import com.javarush.island.trukhanova.entities.base.Plant;
+import com.javarush.island.trukhanova.contracts.ISettings;
+import com.javarush.island.trukhanova.contracts.IWorld;
+import java.util.List;
+
+public class Boar extends EatingHelper {
+    public Boar(int row, int col, ISettings settings) { super(row, col, settings); }
+    @Override
+    public void eat(IWorld world) {
+        tryToEat(world, List.of(Mouse.class, Plant.class));
+    }
+}
+
---
+++ src/main/java/com/javarush/island/trukhanova/entities/herbivores/Rabbit.java
@@ -0,0 +1,19 @@
+package com.javarush.island.trukhanova.entities.herbivores;
+
+import com.javarush.island.trukhanova.entities.base.EatingHelper;
+import com.javarush.island.trukhanova.entities.base.Plant;
+import com.javarush.island.trukhanova.contracts.ISettings;
+import com.javarush.island.trukhanova.contracts.IWorld;
+import java.util.List;
+
+public class Rabbit extends EatingHelper {
+    public Rabbit(int row, int col, ISettings settings) {
+        super(row, col, settings);
+    }
+
+    @Override
+    public void eat(IWorld world) {
+        tryToEat(world, List.of(Plant.class));
+    }
+}
+
